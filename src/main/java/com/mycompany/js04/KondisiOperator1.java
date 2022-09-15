4a83252d99e0b2652d429afd177c89d069a132b4
SupM1cho <76720669+SupM1cho@users.noreply.github.com>
2022-09-15 11:11:40 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/js04/KondisiOperator1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.js04;
+
+//Created by 21343032_Muhammad Rizky Sandyra
+
+public class KondisiOperator1 {
+    public static void main(String[] args) {
+        int score = 0;
+        char answer = 'a';
+        
+        score = (answer == 'b') ? 10 : 0;
+        System.out.println("Score = " + score);
+    }
+}
---
+++ src/main/java/com/mycompany/js04/Tugas1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.js04;
+
+//Created by 21343032_Muhammad Rizky Sandyra
+
+public class Tugas1 {
+    public static void main(String[] args){
+        int numb1 = 10;
+        int numb2 = 20;
+        int numb3 = 45;
+        
+        System.out.println("Number 1 = " + numb1);
+        System.out.println("Number 2 = " + numb2);
+        System.out.println("Number 3 = " + numb3);
+        System.out.println("Average is = " + ((numb1+numb2+numb3)/3));
+    }
+}
